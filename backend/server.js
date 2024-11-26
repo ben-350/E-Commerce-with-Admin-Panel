072ad2be1d76d5db20c70f02f81efc13f86d14eb
ben_j_7 <ben@webcastle.in>
2024-11-26 14:31:52 +0530

    only c added

--- backend/server.js
+++ backend/server.js
@@ -33,7 +33,7 @@ app.use("/api/cart", cartRoutes);
 app.use("/api/coupons", couponRoutes);  
 app.use("/api/payments", paymentRoutes);    
 app.use("/api/analytics", analyticsRoutes);    
-
+//c
 app.listen(PORT, ()=>{
  console.log("Server is running on http://localhost:"+ PORT );
  connectDB();
