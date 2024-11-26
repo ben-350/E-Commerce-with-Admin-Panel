18c031f41541a8d55e305569280f16faeaa6d322
ben_j_7 <ben@webcastle.in>
2024-11-26 14:25:04 +0530

    commit

--- backend/server.js
+++ backend/server.js
@@ -31,13 +31,15 @@ app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);    
 app.use("/api/cart", cartRoutes);    
 app.use("/api/coupons", couponRoutes);    
+
 app.use("/api/payments", paymentRoutes);    
+
+
 app.use("/api/analytics", analyticsRoutes);    
 
-//a
+
 app.listen(PORT, ()=>{
  console.log("Server is running on http://localhost:"+ PORT );
  connectDB();
 })
 
-//b
